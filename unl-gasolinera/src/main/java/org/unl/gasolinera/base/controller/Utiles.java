11e53a8cfee8185bc9ac900af7fc9c11bb23aa1b
vivianaCordova <viviana.e.cordova@unl.edu.ec>
2025-06-13 16:57:06 -0500

    Update Utiles.java
    Signed-off-by: vivianaCordova <viviana.e.cordova@unl.edu.ec>

--- unl-gasolinera/src/main/java/org/unl/gasolinera/base/controller/Utiles.java
+++ unl-gasolinera/src/main/java/org/unl/gasolinera/base/controller/Utiles.java
@@ -25,13 +25,4 @@ public class Utiles {
         }
         return band;
     }
-=======
-public class Utiles {
-    public static final int ASCENDENTE =1;
-    public static final int DESCENDENTE =2;
-    public static Integer START;
-    public static Integer END;
-    public static Integer CONSTIANS;
-    
->>>>>>> Viviana
-}
+
